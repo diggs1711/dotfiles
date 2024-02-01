f5230b52617c2eb37c972ab87b30f205811a0b80
David Higgins <david.higgins7@ucdconnect.ie>
2024-02-01 22:27:30 +0000

    Add zsh config

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,20 @@
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4 | head -5'
+
+# get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3 | head -5'
+
+
+alias ls='ls --color=auto'
